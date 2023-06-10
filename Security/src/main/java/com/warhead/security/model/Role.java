af84586e97b7822c51349b01852d588d3562c199
WARHE8D <murtuzasalman18@gmail.com>
2023-06-10 13:35:07 +0530

    implemented userDetails from springframework.security in user model and added role

---
+++ Security/src/main/java/com/warhead/security/model/Role.java
@@ -0,0 +1,6 @@
+package com.warhead.security.model;
+
+public enum Role {
+    ADMIN,
+    USER
+}
--- Security/src/main/java/com/warhead/security/model/User.java
+++ Security/src/main/java/com/warhead/security/model/User.java
@@ -1,23 +1,56 @@
 package com.warhead.security.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
     @Id
+    @GeneratedValue
     private String id;
     private String username;
     private String password;
     private String email;
     private String firstName;
     private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
