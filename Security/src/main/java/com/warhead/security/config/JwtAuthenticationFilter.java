ff56446733e7f805ddeb14902e9a625ed1031195
WARHE8D <murtuzasalman18@gmail.com>
2023-06-10 15:25:19 +0530

    added JwtService

--- Security/src/main/java/com/warhead/security/config/JwtAuthenticationFilter.java
+++ Security/src/main/java/com/warhead/security/config/JwtAuthenticationFilter.java
@@ -14,11 +14,21 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtSer;
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,
                                     @NonNull HttpServletResponse response,
                                     @NonNull FilterChain filterChain)
             throws ServletException, IOException {
-
+        //first thing in this filter is to check weather the request contains token or not
+        //token are always passed in headers
+        final String authHeader = request.getHeader("AuthVal");
+        final String jwt;
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response); //if no token call to next filter idk y
+            return;
+        }
+        jwt = authHeader.substring(7);//"Bearing " is 8 chars ie index 7 so after this string we have the actual token
+        String userName = jwtSer.getUserName(jwt);
     }
 }
---
+++ Security/src/main/java/com/warhead/security/config/JwtService.java
@@ -0,0 +1,10 @@
+package com.warhead.security.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String getUserName(String jwt) {
+    return null;
+    }
+}
