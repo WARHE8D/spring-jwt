2b573de2083a7fcb539e912c2ae14915de582d2a
WARHE8D <murtuzasalman18@gmail.com>
2023-06-10 18:46:50 +0530

    added extractClaim method

--- Security/src/main/java/com/warhead/security/config/JwtService.java
+++ Security/src/main/java/com/warhead/security/config/JwtService.java
@@ -7,12 +7,13 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
     private static final String SIGNING_KEY = "5558fbbba28bc468507aa45cbf2d74d286cf043ff4ccee9f531b5d8bc5586052";
     public String getUserName(String jwt) {
-    return null;
+    return extractClaim(jwt,Claims::getSubject);//subject will be the userName
     }
     public Claims extractAllClaims(String jwt){
         return Jwts
@@ -28,4 +29,8 @@ public class JwtService {
     return Keys.hmacShaKeyFor(keyByte);
     }
 
+    public <T> T extractClaim(String jwt, Function<Claims,T> claimsResolver){
+        final Claims claims = extractAllClaims(jwt);
+        return claimsResolver.apply(claims);
+    }
 }
