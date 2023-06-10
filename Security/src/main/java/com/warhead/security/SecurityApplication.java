2844df3e2ddc3a6bdced93015f7ad1351a13216e
WARHE8D <murtuzasalman18@gmail.com>
2023-06-10 12:58:24 +0530

    first commit

--- Security/src/main/java/com/warhead/security/SecurityApplication.java
+++ Security/src/main/java/com/warhead/security/SecurityApplication.java
@@ -8,6 +8,7 @@ public class SecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
+		System.out.println("jwt initialized");
 	}
 
 }
