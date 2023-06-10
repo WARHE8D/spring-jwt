d65e72dc53c182d3a53fb38fbfa18c51b6345465
WARHE8D <murtuzasalman18@gmail.com>
2023-06-10 15:50:51 +0530

    added jjwt dependency and created getSigningKey

--- Security/src/main/java/com/warhead/security/config/JwtService.java
+++ Security/src/main/java/com/warhead/security/config/JwtService.java
@@ -1,10 +1,31 @@
 package com.warhead.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+    private static final String SIGNING_KEY = "5558fbbba28bc468507aa45cbf2d74d286cf043ff4ccee9f531b5d8bc5586052";
     public String getUserName(String jwt) {
     return null;
     }
+    public Claims extractAllClaims(String jwt){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJwt(jwt)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+    byte[] keyByte = Decoders.BASE64.decode(SIGNING_KEY);
+    return Keys.hmacShaKeyFor(keyByte);
+    }
+
 }
